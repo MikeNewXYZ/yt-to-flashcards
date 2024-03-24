4b856368816146925aacf13e54128f8dd3a72f8e
NotBaldrick <Git@NotBaldrick.com>
2024-03-24 10:27:18 +0000

    replace fetch url

--- client/src/app.js
+++ client/src/app.js
@@ -13,7 +13,7 @@ const handleSubmit = async (e) => {
     resultsSectionEl.style.display = "none";
     loadingEl.style.removeProperty("display");
 
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://yt-to-flashcards.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
